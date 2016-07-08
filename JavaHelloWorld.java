5659015290786d58b94d0e21cce8e3e30e18448b
Dahai Li <dil@cisco.com>
2016-07-08 14:51:10 -0700

    a simple change

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,5 +1,6 @@
 class JavaHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! This is Java speaking."); // Display the string.
+        System.out.println("This is my last warning.");
     }
 }
